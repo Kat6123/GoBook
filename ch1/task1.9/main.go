f3cfd88ab4a79d048ab409bc8b38786b422848c3
Katerina <kat6123@yandex.ru>
2018-05-07 14:07:32 +0300

    Add task 1.9

---
+++ ch1/task1.9/main.go
@@ -0,0 +1,36 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"net/http"
+)
+
+const httpPrefix  = "http://"
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// Check if url has "http://" prefix
+		if !(strings.HasPrefix(url, httpPrefix)){
+			url = fmt.Sprintf("%s%s", httpPrefix, url)
+		}
+		// Get
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %s\n", err)
+			//os.Exit(1)
+			continue
+		}
+		fmt.Printf("%s Status code: %d\n", url, resp.StatusCode)
+	}
+}
+
+//!-
