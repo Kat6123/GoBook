8928e825ca393f90ee3411a5be8f35d8660017ba
Katerina <kat6123@yandex.ru>
2018-06-09 23:08:20 +0300

    Add ability to save cache site pages locally.

--- ch5/task5.13/findlinks.go
+++ ch5/task5.13/findlinks.go
@@ -9,15 +9,20 @@ import (
 	"os"
 
 	"path/filepath"
+	"strings"
+
+	"io"
+	"net/http"
 
 	"gopl.io/ch5/links"
 )
 
-const (
-	root = "/var/crawler"
-)
+const maxPageCount = 100
 
-var domain string
+var (
+	domain    string
+	pageCount int
+)
 
 // breadthFirst calls f for each item in the worklist.
 // Any items returned by f are added to the worklist.
@@ -37,46 +42,106 @@ func breadthFirst(f func(item string) []string, worklist []string) {
 }
 
 // Do I need to make abstraction under crawl? and pass save as anon func?
-// TODO: add error handling
 func crawl(rawurl string) []string {
-	fmt.Println(rawurl)
-	save(rawurl)
+	log.Printf("visit %s", rawurl)
+
+	if err := save(rawurl); err != nil {
+		log.Printf("save %s as file: %v", rawurl, err)
+		// If an error occurs while saving, then do not follow extracted URLs ?
+		return nil
+	}
 
 	list, err := links.Extract(rawurl)
 	if err != nil {
-		log.Print(err)
+		log.Printf("extract links from %s: %v", rawurl, err)
+		return nil
 	}
 
 	return list
 }
 
-func save(rawurl string) {
+func save(rawurl string) error {
 	u, err := url.Parse(rawurl)
 	if err != nil {
-		log.Printf("parse %s to save as html: %v", rawurl, err)
-		return
+		return fmt.Errorf("parse %s to save as html: %v", rawurl, err)
 	}
+	// It's not an error. But if return nil then links will be extracted from this link?
 	if u.Host != domain {
 		log.Printf("%s hasn't valid domain", u.String())
-		return
+		return nil
+		//return fmt.Errorf("%s hasn't valid domain", u.String())
 	}
 
-	linkPath := filepath.Join(root, domain, u.Path)
-	if _, err := os.Stat(linkPath); os.IsNotExist(err) {
-		os.MkdirAll(linkPath, os.ModePerm)
-		log.Printf("create directory %s", linkPath)
+	dir, file := buildPath(u.Path)
+	if err := createDir(dir); err != nil {
+		return err
+	}
+	if err := saveURL(rawurl, file); err != nil {
+		return err
 	}
 
-	//if strings.HasSuffix(u.Path, "/") {
-	//	linkPath = filepath.Join(linkPath, "index.html")
-	//}
-	//log.Printf("saved at %s", linkPath)
+	log.Printf("saved at %s", file)
+
+	return nil
 }
 
+// createDir creates directory with all above directories if doesn't exist.
+func createDir(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return fmt.Errorf("create directory %s: %v", dir, err)
+		}
+	}
+
+	log.Printf("create directory %s", dir)
+	return nil
+}
+
+func saveURL(url, dest string) error {
+	if pageCount >= maxPageCount {
+		// The crawl ends after it has visited all links in the queue.
+		// It can work with error for a long time. Special type of error or Fatalf will be better decision.
+		return fmt.Errorf("max page count %d was reached", maxPageCount)
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("get file on %s: %v", url, err)
+	}
+	defer resp.Body.Close()
+
+	// Open a file for writing or truncate the old.
+	file, err := os.Create(dest)
+	if err != nil {
+		return fmt.Errorf("create %s: %v", dest, err)
+	}
+	defer file.Close()
+
+	// Use io.Copy to just dump the response body to the file.
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return fmt.Errorf("copy %s to %s: %v", url, dest, err)
+	}
+
+	pageCount++
+	return nil
+}
+
+func buildPath(urlPath string) (dir, file string) {
+	linkPath := filepath.Join(domain, urlPath)
+
+	if strings.HasSuffix(urlPath, "/") {
+		return linkPath, filepath.Join(linkPath, "index.html")
+	} else {
+		dir, _ := filepath.Split(linkPath)
+		return dir, linkPath
+	}
+}
 func main() {
 	// Crawl the web breadth-first,
 	// starting from the command-line arguments.
 
+	// Site url should have trailing slash!
 	u, err := url.Parse(os.Args[1])
 	if err != nil {
 		log.Fatalf("parse url failed: %v", err)
